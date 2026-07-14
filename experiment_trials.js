9ccac5d59769bc2fd32dc61862a646d0f9ccc92b
mariachiaracavagna <74543528+mariachiaracavagna@users.noreply.github.com>
2026-07-14 23:21:32 +0100

    Add files via upload

---
+++ experiment_trials.js
@@ -0,0 +1,325 @@
+// Auto-generated 3-version pilot data (rebuilt from new datasets)
+const TRIALS = {
+  "practice": {
+    "version": "practice",
+    "trial_id": "PRAC",
+    "track_id": "154_Still the Only One_normalized.wav",
+    "ref_eq": "Flat",
+    "ref_url": "https://raw.githubusercontent.com/mariachiaracavagna/survey_segments_7s/main/t154_Flat.mp3",
+    "ctx_urls": [
+      "https://raw.githubusercontent.com/mariachiaracavagna/survey_segments_7s/main/t154_BassCut.mp3",
+      "https://raw.githubusercontent.com/mariachiaracavagna/survey_segments_7s/main/t154_HighCut.mp3",
+      "https://raw.githubusercontent.com/mariachiaracavagna/survey_segments_7s/main/t154_V-Shape.mp3",
+      "https://raw.githubusercontent.com/mariachiaracavagna/survey_segments_7s/main/t154_VocalReduce.mp3"
+    ],
+    "choices": [
+      {
+        "word": "powerful",
+        "method": "clap_baseline"
+      },
+      {
+        "word": "bright",
+        "method": "clap_contextual"
+      },
+      {
+        "word": "flat",
+        "method": "mulan_baseline"
+      },
+      {
+        "word": "grating",
+        "method": "mulan_contextual"
+      }
+    ]
+  },
+  "v1": [
+    {
+      "version": "v1",
+      "trial_id": "A1",
+      "track_id": "276_Strangers on the Grass_normalized.wav",
+      "ref_eq": "Flat",
+      "ref_url": "https://raw.githubusercontent.com/mariachiaracavagna/survey_segments_7s/main/t276_Flat.mp3",
+      "ctx_urls": [
+        "https://raw.githubusercontent.com/mariachiaracavagna/survey_segments_7s/main/t276_BassCut.mp3",
+        "https://raw.githubusercontent.com/mariachiaracavagna/survey_segments_7s/main/t276_HighCut.mp3",
+        "https://raw.githubusercontent.com/mariachiaracavagna/survey_segments_7s/main/t276_V-Shape.mp3",
+        "https://raw.githubusercontent.com/mariachiaracavagna/survey_segments_7s/main/t276_VocalReduce.mp3"
+      ],
+      "choices": [
+        {
+          "word": "sweet",
+          "method": "clap_baseline"
+        },
+        {
+          "word": "sharp",
+          "method": "clap_contextual"
+        },
+        {
+          "word": "fat",
+          "method": "mulan_baseline"
+        },
+        {
+          "word": "powerful",
+          "method": "mulan_contextual"
+        }
+      ]
+    },
+    {
+      "version": "v1",
+      "trial_id": "A2",
+      "track_id": "76_Crazy Glue_normalized.wav",
+      "ref_eq": "BassCut",
+      "ref_url": "https://raw.githubusercontent.com/mariachiaracavagna/survey_segments_7s/main/t76_BassCut.mp3",
+      "ctx_urls": [
+        "https://raw.githubusercontent.com/mariachiaracavagna/survey_segments_7s/main/t76_Flat.mp3",
+        "https://raw.githubusercontent.com/mariachiaracavagna/survey_segments_7s/main/t76_HighCut.mp3",
+        "https://raw.githubusercontent.com/mariachiaracavagna/survey_segments_7s/main/t76_V-Shape.mp3",
+        "https://raw.githubusercontent.com/mariachiaracavagna/survey_segments_7s/main/t76_VocalReduce.mp3"
+      ],
+      "choices": [
+        {
+          "word": "mellow",
+          "method": "clap_baseline"
+        },
+        {
+          "word": "sharp",
+          "method": "clap_contextual"
+        },
+        {
+          "word": "flat",
+          "method": "mulan_baseline"
+        },
+        {
+          "word": "dull",
+          "method": "mulan_contextual"
+        }
+      ]
+    },
+    {
+      "version": "v1",
+      "trial_id": "A3",
+      "track_id": "268_Rudnik_normalized.wav",
+      "ref_eq": "HighCut",
+      "ref_url": "https://raw.githubusercontent.com/mariachiaracavagna/survey_segments_7s/main/t268_HighCut.mp3",
+      "ctx_urls": [
+        "https://raw.githubusercontent.com/mariachiaracavagna/survey_segments_7s/main/t268_BassCut.mp3",
+        "https://raw.githubusercontent.com/mariachiaracavagna/survey_segments_7s/main/t268_Flat.mp3",
+        "https://raw.githubusercontent.com/mariachiaracavagna/survey_segments_7s/main/t268_V-Shape.mp3",
+        "https://raw.githubusercontent.com/mariachiaracavagna/survey_segments_7s/main/t268_VocalReduce.mp3"
+      ],
+      "choices": [
+        {
+          "word": "distant",
+          "method": "clap_baseline"
+        },
+        {
+          "word": "metallic",
+          "method": "clap_contextual"
+        },
+        {
+          "word": "fat",
+          "method": "mulan_baseline"
+        },
+        {
+          "word": "deep",
+          "method": "mulan_contextual"
+        }
+      ]
+    }
+  ],
+  "v2": [
+    {
+      "version": "v2",
+      "trial_id": "B1",
+      "track_id": "159_Una Manana Dorada_normalized.wav",
+      "ref_eq": "V-Shape",
+      "ref_url": "https://raw.githubusercontent.com/mariachiaracavagna/survey_segments_7s/main/t159_V-Shape.mp3",
+      "ctx_urls": [
+        "https://raw.githubusercontent.com/mariachiaracavagna/survey_segments_7s/main/t159_BassCut.mp3",
+        "https://raw.githubusercontent.com/mariachiaracavagna/survey_segments_7s/main/t159_Flat.mp3",
+        "https://raw.githubusercontent.com/mariachiaracavagna/survey_segments_7s/main/t159_HighCut.mp3",
+        "https://raw.githubusercontent.com/mariachiaracavagna/survey_segments_7s/main/t159_VocalReduce.mp3"
+      ],
+      "choices": [
+        {
+          "word": "mellow",
+          "method": "clap_baseline"
+        },
+        {
+          "word": "hollow",
+          "method": "clap_contextual"
+        },
+        {
+          "word": "flat",
+          "method": "mulan_baseline"
+        },
+        {
+          "word": "metallic",
+          "method": "mulan_contextual"
+        }
+      ]
+    },
+    {
+      "version": "v2",
+      "trial_id": "B2",
+      "track_id": "345_EXTRA METAL_normalized.wav",
+      "ref_eq": "VocalReduce",
+      "ref_url": "https://raw.githubusercontent.com/mariachiaracavagna/survey_segments_7s/main/t345_VocalReduce.mp3",
+      "ctx_urls": [
+        "https://raw.githubusercontent.com/mariachiaracavagna/survey_segments_7s/main/t345_BassCut.mp3",
+        "https://raw.githubusercontent.com/mariachiaracavagna/survey_segments_7s/main/t345_Flat.mp3",
+        "https://raw.githubusercontent.com/mariachiaracavagna/survey_segments_7s/main/t345_HighCut.mp3",
+        "https://raw.githubusercontent.com/mariachiaracavagna/survey_segments_7s/main/t345_V-Shape.mp3"
+      ],
+      "choices": [
+        {
+          "word": "fat",
+          "method": "clap_baseline"
+        },
+        {
+          "word": "rumble",
+          "method": "clap_contextual"
+        },
+        {
+          "word": "metallic",
+          "method": "mulan_baseline"
+        },
+        {
+          "word": "distant",
+          "method": "mulan_contextual"
+        }
+      ]
+    },
+    {
+      "version": "v2",
+      "trial_id": "B3",
+      "track_id": "297_Complexo de Inferioridade (com Stray)_normalized.wav",
+      "ref_eq": "Flat",
+      "ref_url": "https://raw.githubusercontent.com/mariachiaracavagna/survey_segments_7s/main/t297_Flat.mp3",
+      "ctx_urls": [
+        "https://raw.githubusercontent.com/mariachiaracavagna/survey_segments_7s/main/t297_BassCut.mp3",
+        "https://raw.githubusercontent.com/mariachiaracavagna/survey_segments_7s/main/t297_HighCut.mp3",
+        "https://raw.githubusercontent.com/mariachiaracavagna/survey_segments_7s/main/t297_V-Shape.mp3",
+        "https://raw.githubusercontent.com/mariachiaracavagna/survey_segments_7s/main/t297_VocalReduce.mp3"
+      ],
+      "choices": [
+        {
+          "word": "dull",
+          "method": "clap_baseline"
+        },
+        {
+          "word": "light",
+          "method": "clap_contextual"
+        },
+        {
+          "word": "brittle",
+          "method": "mulan_baseline"
+        },
+        {
+          "word": "edgy",
+          "method": "mulan_contextual"
+        }
+      ]
+    }
+  ],
+  "v3": [
+    {
+      "version": "v3",
+      "trial_id": "C1",
+      "track_id": "87_Every Time_normalized.wav",
+      "model": "clap_general",
+      "segments": [
+        {
+          "eq": "BassCut",
+          "url": "https://raw.githubusercontent.com/mariachiaracavagna/survey_segments_7s/main/t87_BassCut.mp3",
+          "true_tag": "sharp"
+        },
+        {
+          "eq": "Flat",
+          "url": "https://raw.githubusercontent.com/mariachiaracavagna/survey_segments_7s/main/t87_Flat.mp3",
+          "true_tag": "bright"
+        },
+        {
+          "eq": "HighCut",
+          "url": "https://raw.githubusercontent.com/mariachiaracavagna/survey_segments_7s/main/t87_HighCut.mp3",
+          "true_tag": "muffled"
+        },
+        {
+          "eq": "V-Shape",
+          "url": "https://raw.githubusercontent.com/mariachiaracavagna/survey_segments_7s/main/t87_V-Shape.mp3",
+          "true_tag": "rumble"
+        },
+        {
+          "eq": "VocalReduce",
+          "url": "https://raw.githubusercontent.com/mariachiaracavagna/survey_segments_7s/main/t87_VocalReduce.mp3",
+          "true_tag": "grating"
+        }
+      ]
+    },
+    {
+      "version": "v3",
+      "trial_id": "C2",
+      "track_id": "291_Complexo de Inferioridade (com Stray)_normalized.wav",
+      "model": "MuQMuLan",
+      "segments": [
+        {
+          "eq": "BassCut",
+          "url": "https://raw.githubusercontent.com/mariachiaracavagna/survey_segments_7s/main/t291_BassCut.mp3",
+          "true_tag": "rich"
+        },
+        {
+          "eq": "Flat",
+          "url": "https://raw.githubusercontent.com/mariachiaracavagna/survey_segments_7s/main/t291_Flat.mp3",
+          "true_tag": "edgy"
+        },
+        {
+          "eq": "HighCut",
+          "url": "https://raw.githubusercontent.com/mariachiaracavagna/survey_segments_7s/main/t291_HighCut.mp3",
+          "true_tag": "dark"
+        },
+        {
+          "eq": "V-Shape",
+          "url": "https://raw.githubusercontent.com/mariachiaracavagna/survey_segments_7s/main/t291_V-Shape.mp3",
+          "true_tag": "harsh"
+        },
+        {
+          "eq": "VocalReduce",
+          "url": "https://raw.githubusercontent.com/mariachiaracavagna/survey_segments_7s/main/t291_VocalReduce.mp3",
+          "true_tag": "mellow"
+        }
+      ]
+    },
+    {
+      "version": "v3",
+      "trial_id": "C3",
+      "track_id": "143_You & My Friend_normalized.wav",
+      "model": "clap_general",
+      "segments": [
+        {
+          "eq": "BassCut",
+          "url": "https://raw.githubusercontent.com/mariachiaracavagna/survey_segments_7s/main/t143_BassCut.mp3",
+          "true_tag": "sharp"
+        },
+        {
+          "eq": "Flat",
+          "url": "https://raw.githubusercontent.com/mariachiaracavagna/survey_segments_7s/main/t143_Flat.mp3",
+          "true_tag": "balanced"
+        },
+        {
+          "eq": "HighCut",
+          "url": "https://raw.githubusercontent.com/mariachiaracavagna/survey_segments_7s/main/t143_HighCut.mp3",
+          "true_tag": "distant"
+        },
+        {
+          "eq": "V-Shape",
+          "url": "https://raw.githubusercontent.com/mariachiaracavagna/survey_segments_7s/main/t143_V-Shape.mp3",
+          "true_tag": "sweet"
+        },
+        {
+          "eq": "VocalReduce",
+          "url": "https://raw.githubusercontent.com/mariachiaracavagna/survey_segments_7s/main/t143_VocalReduce.mp3",
+          "true_tag": "rumble"
+        }
+      ]
+    }
+  ]
+};
